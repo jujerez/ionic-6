e593739226be79253654c35c389f9fcf7adcb0a2
Juan Antonio <juanantonio.jerez@iesdonana.org>
2021-12-08 19:04:17 +0100

    Importación de IonicModule

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
@@ -8,7 +9,8 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   exports: [
     HeaderComponent
