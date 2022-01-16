dc060a98b5288db5d3f36f826d2329f939bf5450
Juan Antonio <juanantonio.jerez@iesdonana.org>
2022-01-16 18:12:29 +0100

    ion-avatar

--- src/app/pages/avatar/avatar.module.ts
+++ src/app/pages/avatar/avatar.module.ts
@@ -7,14 +7,20 @@ import { IonicModule } from '@ionic/angular';
 import { AvatarPageRoutingModule } from './avatar-routing.module';
 
 import { AvatarPage } from './avatar.page';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AvatarPageRoutingModule
+    AvatarPageRoutingModule,
+    ComponentsModule,
+  
   ],
-  declarations: [AvatarPage]
+  declarations: [AvatarPage],
+  exports: [HeaderComponent]
+  
 })
 export class AvatarPageModule {}
